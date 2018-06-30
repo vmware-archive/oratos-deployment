f5e5f43ab56e1079c555fc7dd897efa264aaeda1
DennyZhang <denny.zhang001@gmail.com>
2018-06-30 00:03:22 -0700

    shellcheck fix

--- secrets/generate-tls-certs.sh
+++ secrets/generate-tls-certs.sh
@@ -13,16 +13,18 @@ data:
 '
 
 b64cmd=base64
-if [ "$(uname | tr A-Z a-z)" = "linux" ]; then
+if [ "$(uname | tr '[:upper:]' '[:lower:]')" = "linux" ]; then
   b64cmd="base64 -w 0"
 fi
 
 docker run -v "$PWD/loggregator-tls-certs:/output" loggregator/certs
-if [ "$(uname | tr A-Z a-z)" = "linux" ]; then
+if [ "$(uname | tr '[:upper:]' '[:lower:]')" = "linux" ]; then
   sudo chown -R "$(whoami)" loggregator-tls-certs
 fi
+
 echo "$tls_certs_header" > loggregator-tls-certs.yml
-ls loggregator-tls-certs |
-    while read line
-        do echo "  $line: $(cat loggregator-tls-certs/$line | $b64cmd)"
-    done >> loggregator-tls-certs.yml
+for f in loggregator-tls-certs/*; do
+    fname=$(basename "$f")
+    content=$("$b64cmd" "$f")
+    echo "  $fname: $content" >> loggregator-tls-certs.yml
+done
